e273e81aed190d0feda20c936ada41292a58072d
Mher-777 <mherjan2003@mail.ru>
2019-12-24 21:41:57 +0400

    added product-slider and product

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,17 @@
 $(function () {
 
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: '12px',
+        readOnly: true,
+    });
+    $('.product-slider__inner').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        dots: true,
+        arrows: false,
+        autoplay: true
+    });
     var mixer = mixitup('.products__inner-box');
     
 });
